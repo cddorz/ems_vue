c7454a0533c7dd69642523df44695fa24bcc724d
cddorz <2878153350@qq.com>
2021-03-20 00:07:24 +0800

    redis缓存实现

---
+++ src/main/java/com/hly/cache/RedisCache.java
@@ -0,0 +1,8 @@
+package com.hly.cache;
+
+/**
+ * @author han long yi
+ * @create 2021-03-19 22:00
+ */
+public class RedisCache {
+}
---
+++ src/main/java/com/hly/utils/ApplicationContextUtils.java
@@ -0,0 +1,5 @@
+package com.hly.utils;/**
+@author han long yi
+@create 2021-03-19 22:05
+*/public class ApplicationContextUtils {
+}
